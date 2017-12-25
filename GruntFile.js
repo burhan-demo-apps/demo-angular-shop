b25670ed68f86dec539ceec366f295a51524dd4f
Burhan Otour <burhan.otour@gmail.com>
2017-12-25 14:38:09 +0100

    Add basic configuration to organize JS files (contact, uglify)

--- GruntFile.js
+++ GruntFile.js
@@ -1,23 +1,62 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
+    'use strict';
+    // Declare JS dependencies and third-party libraries required
+    var jsVendorSourceFiles = [
+        'app/bower_components/jquery/jquery.js',
+        'app/bower_components/lodash/dist/lodash.js',
+        'app/bower_components/handlebars/handlebars.js',
+        'app/bower_components/ember/ember.js',
+        'app/bower_components/accounting/accounting.js',
+        'app/bower_components/moment/moment.js'
+    ];
+
+    // Declare the App's source files
+    var jsAppSourceFiles = [
+        'app/js-src/app.js'
+    ];
+
+    // Concatenate our file arrays to create a bundle we can use later
+    var jsSourceFiles = jsVendorSourceFiles.concat(jsAppSourceFiles);
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
+        jshint: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                jshintrc: 'js-src/.jshintrc'
             },
-            build: {
-                src: ['bower_components/angular/angular.min.js', 'app/js/*.js'],
-                dest: 'build/js/app.js'
+            files: {
+                src: jsAppSourceFiles
             }
-        }
+        },
+        concat: {
+            dev: {
+                files: [
+                    {
+                        src: jsSourceFiles,
+                        dest: 'js/app.js'
+                    }
+                ]
+            }
+        },
+        uglify: {
+            prod: {
+                files: [
+                    {
+                        src: jsSourceFiles,
+                        dest: 'app/js/app.js'
+                    }
+                ]
+            },
+        },
     });
 
-    // Load the plugin that provides the "uglify" task.
+    // Load the plugin tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
-
+    grunt.registerTask('default', ['jshint', 'concat:dev']);
+    grunt.registerTask('build-prod', ['jshint', 'uglify:prod']);
 };
